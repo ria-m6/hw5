4e18f2646ecca26eb648ea388cdecb83e3eba4f9
Ria Murali <riamural@usc.edu>
2025-04-24 18:56:22 +0000

    wordle and schedule working

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+typedef vector<size_t> ShiftCount;
 
+bool schedule_helper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, ShiftCount& shifts, size_t day, size_t slot);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +40,72 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t numDays = avail.size();
+    if(dailyNeed == 0){
+      return false;
+    }
+    size_t numWorkers = avail[0].size();
+
+    //initializing schedule with INVALID_ID
+    sched.assign(numDays, vector<Worker_T>(dailyNeed, INVALID_ID));
+    ShiftCount shifts(numWorkers, 0);
+
+    return schedule_helper(avail, dailyNeed, maxShifts, sched, shifts, 0, 0);
+
+}
+
+
+//recursive function
+bool schedule_helper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, ShiftCount& shifts, size_t day, size_t slot){
+  
+  size_t numDays = avail.size();
+  size_t numWorkers = avail[0].size();
+
+  //if all the days are scheduled
+  if(day == numDays){
+    return true;
+  }
+
+  //if today's slots are all filled up, then move to the next day
+  if(slot == dailyNeed){
+    return schedule_helper(avail, dailyNeed, maxShifts, sched, shifts, day+1, 0);
+  }
+
+  //try each workers for current day and slot
+  for(Worker_T w =0; w<numWorkers; ++w){
+    if(!avail[day][w] || shifts[w]>=maxShifts){
+      continue;
+    }
+    //making sure that they havent already been assigned to this day
+    bool used = false;
+    for(size_t s =0; s<slot; ++s){
+      if(sched[day][s]==w){
+        used = true;
+        break;
+      }
+    }
+    if(used){
+      continue;
+    }
+
+    //assign the worker way
+    sched[day][slot] = w;
+    ++shifts[w];
+
+    //recurse to next slot
+    if(schedule_helper(avail, dailyNeed, maxShifts, sched, shifts, day, slot+1)){
+      return true;
+    }
+
+    //Backtrack
+    --shifts[w];
+    sched[day][slot]=INVALID_ID;
+  }
+  
 
 
 
+
+  return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+static void fill_helper(string &pattern, int pos, string floating, const set<string> &dict, set<string> & results);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,47 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> results;
+    // making a copy of input pattern
+    string pattern =in;
+    //start from 0
+    fill_helper(pattern, 0, floating, dict, results);
 
+    return results;
+  
 }
 
 // Define any helper functions here
+static void fill_helper(string &pattern, int pos, string floating, const set<string> &dict, set<string> & results){
+  int n = pattern.size();
+  //if all positions are placed
+  if(pos == n){
+    //all floating letters must be used
+    if(floating.empty() && dict.find(pattern) != dict.end()){
+      results.insert(pattern);
+    }
+    return;
+  }
+
+  //if this position is fixed, move on
+  if(pattern[pos]!= '-'){
+    fill_helper(pattern, pos+1, floating, dict, results);
+  } else{
+    //Try every possible letter 
+    for(char c ='a';c<='z';c++){
+      //place letter at curr blank
+      pattern[pos] = c;
+      //if c is req floating letter, 
+      string nextFloating= floating;
+      auto it = nextFloating.find(c);
+      if(it != string::npos){
+        nextFloating.erase(it, 1);
+      }
+      //recurse to fill next position
+      fill_helper(pattern, pos+1, nextFloating, dict, results);
+    }
+
+    //restores blank for other branhces
+    pattern[pos] = '-';
+  }
+}
